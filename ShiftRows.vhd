a8eede2a8f85448ad846c5934beae8fba1857634
Lars Refsgaard <larsrefsgaard@LarsComputer.local>
2021-01-05 12:39:29 +0100

    fix .gitignore and ShiftRows.vhd

--- ShiftRows.vhd
+++ ShiftRows.vhd
@@ -1,48 +1,44 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
-entity ShiftRows is
-	
-	port(
-			
-			input : in std_logic_vector(127 downto 0);
-			output : out std_logic_vector(127 downto 0)
-			);
-			
-end entity;
+ENTITY ShiftRows IS
 
+	PORT (
 
+		input : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
+		output : OUT STD_LOGIC_VECTOR(127 DOWNTO 0)
+	);
+
+END ENTITY;
 
 --=======================================================
 -- Architecture declaration
 --=======================================================
 
-architecture ShiftRows_arch of ShiftRows is
+ARCHITECTURE ShiftRows_arch OF ShiftRows IS
 
-begin 
+BEGIN
 
 	-- Shifting 1st row
-    output(127 downto 96) <= input(127 downto 96);
+	output(127 DOWNTO 96) <= input(127 DOWNTO 96);
 
-    -- Shifting 2nd row
-    output(95 downto 88) <= input(87 downto 80);
-    output(87 downto 80) <= input(79 downto 72);
-    output(79 downto 72) <= input(71 downto 64);
-    output(71 downto 64) <= input(95 downto 88);
+	-- Shifting 2nd row
+	output(95 DOWNTO 88) <= input(87 DOWNTO 80);
+	output(87 DOWNTO 80) <= input(79 DOWNTO 72);
+	output(79 DOWNTO 72) <= input(71 DOWNTO 64);
+	output(71 DOWNTO 64) <= input(95 DOWNTO 88);
 
-    -- Shifting 3rd row
-    output(63 downto 56) <= input(47 downto 40);
-    output(55 downto 48) <= input(39 downto 32);
-    output(47 downto 40) <= input(63 downto 56);
-    output(39 downto 32) <= input(55 downto 48);
+	-- Shifting 3rd row
+	output(63 DOWNTO 56) <= input(47 DOWNTO 40);
+	output(55 DOWNTO 48) <= input(39 DOWNTO 32);
+	output(47 DOWNTO 40) <= input(63 DOWNTO 56);
+	output(39 DOWNTO 32) <= input(55 DOWNTO 48);
 
-    -- Shifting 4th row
-    output(31 downto 24) <= input(7 downto 0);
-    output(23 downto 16) <= input(31 downto 24);
-    output(15 downto 8) <= input(23 downto 16);
-    output(7 downto 0) <= input(15 downto 8);
-
-
-end architecture;
+	-- Shifting 4th row
+	output(31 DOWNTO 24) <= input(7 DOWNTO 0);
+	output(23 DOWNTO 16) <= input(31 DOWNTO 24);
+	output(15 DOWNTO 8) <= input(23 DOWNTO 16);
+	output(7 DOWNTO 0) <= input(15 DOWNTO 8);
+END ARCHITECTURE;
