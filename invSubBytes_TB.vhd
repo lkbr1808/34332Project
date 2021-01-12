f8020c7b75bba67f4ee1a4c31845fae1e39e271b
Gustav Surface <s183934>
2021-01-12 09:47:05 +0100

    Refactor 2: Electric Boogaloo

--- invSubBytes_TB.vhd
+++ invSubBytes_TB.vhd
@@ -1,4 +1,3 @@
-
 LIBRARY ieee;
 LIBRARY STD;
 USE STD.textio.ALL;
@@ -7,18 +6,16 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE ieee.std_logic_unsigned.ALL;
 
-ENTITY InverseSubBytes_TB IS
+ENTITY invSubBytes_TB IS
 END ENTITY;
 
-ARCHITECTURE InverseSubBytes_TB_arc OF InverseSubBytes_TB IS
-
-
-	COMPONENT InverseSubBytes IS
-	PORT (
-		clk, in_val      : IN STD_LOGIC;
-		data_in  : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
-		out_val  : OUT STD_LOGIC;
-		data_out : OUT STD_LOGIC_VECTOR(127 DOWNTO 0));
+ARCHITECTURE invSubBytes_TB_arc OF invSubBytes_TB IS
+	COMPONENT invSubBytes IS
+		PORT (
+			clk, in_val : IN STD_LOGIC;
+			data_in : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
+			out_val : OUT STD_LOGIC;
+			data_out : OUT STD_LOGIC_VECTOR(127 DOWNTO 0));
 
 	END COMPONENT;
 
@@ -34,12 +31,12 @@ BEGIN
 		WAIT FOR 5 ns;
 	END PROCESS;
 
-	DUT : InverseSubBytes PORT MAP(clk, in_val_TB, data_in_TB, out_val_TB, data_out_TB);
+	DUT : invSubBytes PORT MAP(clk, in_val_TB, data_in_TB, out_val_TB, data_out_TB);
 
 	PROCESS BEGIN
-	data_in_TB <= x"10a58869d74be5a374cf867cfb473859";
-	in_val_TB <= '1';
-	wait for 30 ns;
+		data_in_TB <= x"10a58869d74be5a374cf867cfb473859";
+		in_val_TB <= '1';
+		WAIT FOR 30 ns;
 	END PROCESS;
 
 END ARCHITECTURE;
